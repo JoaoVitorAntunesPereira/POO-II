979aadc163e43e39e1365d71dbf6415ee4ae9541
João Vitor <114810543+JoaoVitorAntunesPereira@users.noreply.github.com>
2025-04-18 18:41:01 -0300

    att

--- aulas/aula_05/main.py
+++ aulas/aula_05/main.py
@@ -3,8 +3,8 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from aula_04.conta_property import Transacao, Conta
-from nicegui import ui
+from conta_property_2 import Transacao, Conta
+from nicegui import ui, app, observables
 from datetime import datetime
 from rich import print
 
@@ -13,26 +13,47 @@ from rich import print
 contas: list[Conta] = []
 
 
+
+
+
+@ui.page('/cadastrar_conta')
 def criar_conta():
-    conta = Conta(saldo=float(saldo.value), pix=pix.value, limite=float(limite.value))
-    contas.append(conta)
+    
+    def criar_conta_func():
+        ui.navigate.to('/')
+        conta = Conta(saldo=float(saldo.value), pix=pix.value, limite=float(limite.value), nome=nome.value)
+        ui.notify(conta.saldo)
+        contas.append(conta)
+        app.storage.general(contas)
+        
+    
+    with ui.column():
+        ui.label("Criar conta")
+        saldo = ui.input(label='Saldo da conta')
+        nome = ui.input(label='Nome')
+        pix = ui.input(label='Chave pix')
+        limite = ui.input(label='Limite')
+        ui.button('Criar', on_click=criar_conta_func)
 
 
-
-with ui.column():
-    ui.label("Criar conta")
-    saldo = ui.input(label='Saldo da conta')
-    nome = ui.input(label='Nome')
-    pix = ui.input(label='Chave pix')
-    limite = ui.input(label='Limite')
-    ui.button('Criar', on_click=criar_conta)
-
-with ui.column():
+@ui.page('/')
+def contas_cadastradas():
+    ui.link('Página inicial', '/', new_tab=False)
+    ui.label('Contas cadastradas')
+    
+    
+    columns = [
+        {'name': 'nome', 'label': 'Nome', 'field': 'nome', 'align': 'left'},
+        {'name': 'pix', 'label': 'Chave pix', 'field': 'pix'}
+    ]
+    
+    rows = []
+    
     for conta in contas:
-        ui.label(f"{conta.nome}")
-
-
-
-
+        rows.append({'nome': conta.nome, 'pix': conta.pix})
+        
+    ui.table(columns=columns, rows=rows, row_key='name')
+    
+    ui.button('Cadastrar conta', on_click=lambda: ui.navigate.to('/cadastrar_conta'))
 
 ui.run()
